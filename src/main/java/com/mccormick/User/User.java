4e6fb787930cc7bb372f58f728e5adebf3afc41d
Alexander Kruglov <alexkrug524@gmail.com>
2025-11-25 13:05:37 +0300

    refactor User class

---
+++ src/main/java/com/mccormick/User/User.java
@@ -0,0 +1,56 @@
+package com.mccormick.User;
+
+// Made by alexkrug
+public class User {
+    private String userName;
+    private String password;
+
+    private int userAge;
+    private String gender;
+    private String city;
+
+    public User(String userName, String password) { //Constructor
+        this.userName = userName;
+        this.password = password;
+    }
+
+    public String getUserName() {
+        return userName;
+    }
+
+    public void setUserName(String userName) {
+        this.userName = userName;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    public int getUserAge() {
+        return userAge;
+    }
+
+    public void setUserAge(int userAge) {
+        this.userAge = userAge;
+    }
+
+    public String getCity() {
+        return city;
+    }
+
+    public void setCity(String city) {
+        this.city = city;
+    }
+
+    public String getGender() {
+        return gender;
+    }
+
+    public void setGender(String gender) {
+        this.gender = gender;
+    }
+}
