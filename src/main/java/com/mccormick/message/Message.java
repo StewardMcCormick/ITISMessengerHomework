c8bc7971072be91ede6c2323e2853b1bbaf9747e
Nick-labs <nikita.fomin.0404@ya.ru>
2025-11-26 21:17:03 +0300

    Add Message.java

---
+++ src/main/java/com/mccormick/message/Message.java
@@ -0,0 +1,35 @@
+package com.mccormick.message;
+
+import com.mccormick.User.User;
+
+import java.util.Date;
+
+public class Message {
+    private final String text;
+    private final Date date;
+    private final User sender;
+    private final User receiver;
+
+    public Message(String text, User sender, User receiver) {
+        this.text = text;
+        this.date = new Date();
+        this.sender = sender;
+        this.receiver = receiver;
+    }
+
+    public String getText() {
+        return text;
+    }
+
+    public Date getDate() {
+        return date;
+    }
+
+    public User getSender() {
+        return sender;
+    }
+
+    public User getReceiver() {
+        return receiver;
+    }
+}
