2f7ad030eaaec45cf7fab779d48e93bc3630f34a
Nick-labs <nikita.fomin.0404@ya.ru>
2025-11-26 22:49:12 +0300

    Add MessageDatabase.java

---
+++ src/main/java/com/mccormick/message/MessagesDatabase.java
@@ -0,0 +1,42 @@
+package com.mccormick.message;
+
+import com.mccormick.User.User;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class MessagesDatabase {
+    private List<Message> messageList = new ArrayList<>();
+
+    public void addNewMessage(Message message) {
+        messageList.add(message);
+    }
+
+    public List<Message> getAllMessages() {
+        return messageList;
+    }
+
+    public List<Message> getMessagesBySender(User user) {
+        List<Message> resultList = new ArrayList<>();
+
+        for (Message m : messageList) {
+            if (m.getSender() == user) {
+                resultList.add(m);
+            }
+        }
+
+        return resultList;
+    }
+
+    public List<Message> getMessagesByReceiver(User user) {
+        List<Message> resultList = new ArrayList<>();
+
+        for (Message m : messageList) {
+            if (m.getReceiver() == user) {
+                resultList.add(m);
+            }
+        }
+
+        return resultList;
+    }
+}
